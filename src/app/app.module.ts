17f8cdec7afac624afc6995b40ca2acb83bf6bcb
Eray S <eray_075@msn.com>
2020-11-04 11:31:04 +0100

    Added ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { environment } from 'src/environments/environment';
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [ 
     NgxsModule.forRoot([AppState], {
       developmentMode: !environment.production
     }),
