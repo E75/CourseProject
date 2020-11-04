97f963a35dab73d9fa414af93169c7fe51e41200
Eray S <eray_075@msn.com>
2020-11-04 11:24:56 +0100

    Added ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {AppState} from './app.state';
 import { AppComponent } from './app.component';
+import {NgxsModule} from '@ngxs/store';
+import { environment } from 'src/environments/environment';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgxsModule.forRoot([AppState], {
+      developmentMode: !environment.production
+    }),
     BrowserModule
   ],
   providers: [],
